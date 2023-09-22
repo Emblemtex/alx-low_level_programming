118cc51dced486c117d28982915d98fc279804a7
emblemtex <kantunna1808@gmail.com>
2023-09-22 14:31:39 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *scr);
 char *_strncat(char *dest, char *scr, int n);
@@ -8,9 +11,9 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
-char *cap_string(char *a);
-char *leet(char *a);
-char *rot13(char *a);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
